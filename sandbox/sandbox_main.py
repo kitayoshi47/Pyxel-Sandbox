19eb3592606c3bde38cf5b8b6042cfafe6feaf39
kitayoshi47 <nyamo85@gmail.com>
2024-12-05 14:36:55 +0900

    test

--- sandbox/sandbox_main.py
+++ sandbox/sandbox_main.py
@@ -9,7 +9,7 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, title="Hello Pyxel !!!!!")
+        pyxel.init(160, 120, title="Hello Pyxel")
         pyxel.images[0].load(0, 0, "assets/pyxel_logo_38x16.png")
         pyxel.run(self.update, self.draw)
 
@@ -19,6 +19,6 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.text(46, 41, "Hello, Pyxel (^-^)", pyxel.frame_count % 16)
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 App()
